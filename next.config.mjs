f77bd317618623070ebff5ea6782b387856d9867
Hcaglar32 <hasan.49.50@hotmail.com>
2024-06-25 21:46:12 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/hasan-emir-caglar",
     // Static export enabled
     output: "export",
     // Enable React's strict mode
