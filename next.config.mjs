33ee722f4e1c6f99d5c89d106eff594b26ac8b16
Hcaglar32 <hasan.49.50@hotmail.com>
2024-06-25 21:48:15 +0300

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Static export enabled
-    output: "export",
-    // Enable React's strict mode
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
-  
+    output:"export",
+};
+
+export default nextConfig;
